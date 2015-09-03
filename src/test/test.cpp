ba65614a5dc7560ab40b24972f5b52f39573fc7a
Alessandro Gangemi <agangemi@stevens.edu>
2015-09-02 22:41:13 -0400

    Add unit tests for Entity mutators

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -15,3 +15,19 @@ TEST(EntityConstructorTest, IDNameConstructor)
 	ASSERT_TRUE(e.id() == 42);
 	ASSERT_TRUE(e.name().compare("Runcible Spoon") == 0);
 }
+
+TEST(EntityMutatorTest, IDMutatorTest)
+{
+	StevensDev::Entity e = StevensDev::Entity();
+	ASSERT_TRUE(e.id() == 0);
+	e.setID(127);
+	ASSERT_TRUE(e.id() == 127);
+}
+
+TEST(EntityMutatorTest, NameMutatorTest)
+{
+	StevensDev::Entity e = StevensDev::Entity();
+	ASSERT_TRUE(e.name().compare("") == 0);
+	e.setName("Vorpal Sword");
+	ASSERT_TRUE(e.name().compare("Vorpal Sword") == 0);
+}
