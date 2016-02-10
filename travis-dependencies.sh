f811a19228cc5794e220458149e3c8ebd38cd851
Alessandro Gangemi <agangemi@stevens.edu>
2016-02-09 19:45:40 -0500

    travis: Don't build new cmake

--- travis-dependencies.sh
+++ travis-dependencies.sh
@@ -8,7 +8,7 @@ fi
 
 sudo add-apt-repository -y ppa:george-edison55/precise-backports
 sudo apt-get update -qq
-sudo apt-get install -y libgtest-dev cmake
+sudo apt-get install -y libgtest-dev
 sudo mkdir /usr/src/gtest/build
 cd /usr/src/gtest/build
 sudo cmake .. -DBUILD_SHARED_LIBS=1
